db17c1aec905183dd7da9651314d685431453fc7
wang3624270 <3624270@qq.com>
2018-06-16 15:27:56 +0800

    2018/6/16

--- config/index.js
+++ config/index.js
@@ -6,12 +6,18 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
-
+    proxyTable: {
+      '/onlinelearning':{
+        target:'http://localhost:8080/onlinelearning',
+        changeOrigin:true,
+        pathRewrite :{
+          '^/onlinelearning':'',
+        }
+      }
+    },
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
